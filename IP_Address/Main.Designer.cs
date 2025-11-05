c0b60553b4f14aef4762efc283f628857fc7bac1
Veronika050987 <veronicaviv@mail.ru>
2025-11-05 20:51:20 +0300

    Add project files.

---
+++ IP_Address/Main.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace IP_Address
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TextBox_IPAddress = new System.Windows.Forms.TextBox();
+			this.labelIP = new System.Windows.Forms.Label();
+			this.TextBox_SubnetMask = new System.Windows.Forms.TextBox();
+			this.labelSubnetMask = new System.Windows.Forms.Label();
+			this.Button_Calculate = new System.Windows.Forms.Button();
+			this.Label_NetworkAddress = new System.Windows.Forms.Label();
+			this.Label_BroadcastAddress = new System.Windows.Forms.Label();
+			this.Label_TotalIPAddresses = new System.Windows.Forms.Label();
+			this.Label_UsableIPAddresses = new System.Windows.Forms.Label();
+			this.textBox_NetworkAddress = new System.Windows.Forms.TextBox();
+			this.textBox_BroadcastAddress = new System.Windows.Forms.TextBox();
+			this.textBox_TotalIPAddresses = new System.Windows.Forms.TextBox();
+			this.textBox_UsableIPAddresses = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// TextBox_IPAddress
+			// 
+			this.TextBox_IPAddress.Location = new System.Drawing.Point(190, 39);
+			this.TextBox_IPAddress.Name = "TextBox_IPAddress";
+			this.TextBox_IPAddress.Size = new System.Drawing.Size(268, 22);
+			this.TextBox_IPAddress.TabIndex = 0;
+			// 
+			// labelIP
+			// 
+			this.labelIP.AutoSize = true;
+			this.labelIP.Location = new System.Drawing.Point(36, 42);
+			this.labelIP.Name = "labelIP";
+			this.labelIP.Size = new System.Drawing.Size(75, 16);
+			this.labelIP.TabIndex = 1;
+			this.labelIP.Text = "IP address:";
+			// 
+			// TextBox_SubnetMask
+			// 
+			this.TextBox_SubnetMask.Location = new System.Drawing.Point(190, 94);
+			this.TextBox_SubnetMask.Name = "TextBox_SubnetMask";
+			this.TextBox_SubnetMask.Size = new System.Drawing.Size(268, 22);
+			this.TextBox_SubnetMask.TabIndex = 2;
+			// 
+			// labelSubnetMask
+			// 
+			this.labelSubnetMask.AutoSize = true;
+			this.labelSubnetMask.Location = new System.Drawing.Point(36, 97);
+			this.labelSubnetMask.Name = "labelSubnetMask";
+			this.labelSubnetMask.Size = new System.Drawing.Size(88, 16);
+			this.labelSubnetMask.TabIndex = 3;
+			this.labelSubnetMask.Text = "Subnet mask:";
+			// 
+			// Button_Calculate
+			// 
+			this.Button_Calculate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.Button_Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Button_Calculate.Location = new System.Drawing.Point(152, 146);
+			this.Button_Calculate.Name = "Button_Calculate";
+			this.Button_Calculate.Size = new System.Drawing.Size(105, 30);
+			this.Button_Calculate.TabIndex = 4;
+			this.Button_Calculate.Text = "Calculate";
+			this.Button_Calculate.UseVisualStyleBackColor = false;
+			this.Button_Calculate.Click += new System.EventHandler(this.Button_Calculate_Click);
+			// 
+			// Label_NetworkAddress
+			// 
+			this.Label_NetworkAddress.AutoSize = true;
+			this.Label_NetworkAddress.Location = new System.Drawing.Point(36, 195);
+			this.Label_NetworkAddress.Name = "Label_NetworkAddress";
+			this.Label_NetworkAddress.Size = new System.Drawing.Size(112, 16);
+			this.Label_NetworkAddress.TabIndex = 5;
+			this.Label_NetworkAddress.Text = "Network address:";
+			// 
+			// Label_BroadcastAddress
+			// 
+			this.Label_BroadcastAddress.AutoSize = true;
+			this.Label_BroadcastAddress.Location = new System.Drawing.Point(36, 238);
+			this.Label_BroadcastAddress.Name = "Label_BroadcastAddress";
+			this.Label_BroadcastAddress.Size = new System.Drawing.Size(125, 16);
+			this.Label_BroadcastAddress.TabIndex = 6;
+			this.Label_BroadcastAddress.Text = "Broadcast address:";
+			// 
+			// Label_TotalIPAddresses
+			// 
+			this.Label_TotalIPAddresses.AutoSize = true;
+			this.Label_TotalIPAddresses.Location = new System.Drawing.Point(36, 280);
+			this.Label_TotalIPAddresses.Name = "Label_TotalIPAddresses";
+			this.Label_TotalIPAddresses.Size = new System.Drawing.Size(124, 16);
+			this.Label_TotalIPAddresses.TabIndex = 7;
+			this.Label_TotalIPAddresses.Text = "Total IP addresses:";
+			// 
+			// Label_UsableIPAddresses
+			// 
+			this.Label_UsableIPAddresses.AutoSize = true;
+			this.Label_UsableIPAddresses.Location = new System.Drawing.Point(36, 322);
+			this.Label_UsableIPAddresses.Name = "Label_UsableIPAddresses";
+			this.Label_UsableIPAddresses.Size = new System.Drawing.Size(137, 16);
+			this.Label_UsableIPAddresses.TabIndex = 8;
+			this.Label_UsableIPAddresses.Text = "Usable IP addresses:";
+			// 
+			// textBox_NetworkAddress
+			// 
+			this.textBox_NetworkAddress.Location = new System.Drawing.Point(190, 195);
+			this.textBox_NetworkAddress.Name = "textBox_NetworkAddress";
+			this.textBox_NetworkAddress.Size = new System.Drawing.Size(268, 22);
+			this.textBox_NetworkAddress.TabIndex = 9;
+			// 
+			// textBox_BroadcastAddress
+			// 
+			this.textBox_BroadcastAddress.Location = new System.Drawing.Point(190, 238);
+			this.textBox_BroadcastAddress.Name = "textBox_BroadcastAddress";
+			this.textBox_BroadcastAddress.Size = new System.Drawing.Size(268, 22);
+			this.textBox_BroadcastAddress.TabIndex = 10;
+			// 
+			// textBox_TotalIPAddresses
+			// 
+			this.textBox_TotalIPAddresses.Location = new System.Drawing.Point(190, 277);
+			this.textBox_TotalIPAddresses.Name = "textBox_TotalIPAddresses";
+			this.textBox_TotalIPAddresses.Size = new System.Drawing.Size(268, 22);
+			this.textBox_TotalIPAddresses.TabIndex = 11;
+			// 
+			// textBox_UsableIPAddresses
+			// 
+			this.textBox_UsableIPAddresses.Location = new System.Drawing.Point(190, 319);
+			this.textBox_UsableIPAddresses.Name = "textBox_UsableIPAddresses";
+			this.textBox_UsableIPAddresses.Size = new System.Drawing.Size(268, 22);
+			this.textBox_UsableIPAddresses.TabIndex = 12;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox_UsableIPAddresses);
+			this.Controls.Add(this.textBox_TotalIPAddresses);
+			this.Controls.Add(this.textBox_BroadcastAddress);
+			this.Controls.Add(this.textBox_NetworkAddress);
+			this.Controls.Add(this.Label_UsableIPAddresses);
+			this.Controls.Add(this.Label_TotalIPAddresses);
+			this.Controls.Add(this.Label_BroadcastAddress);
+			this.Controls.Add(this.Label_NetworkAddress);
+			this.Controls.Add(this.Button_Calculate);
+			this.Controls.Add(this.labelSubnetMask);
+			this.Controls.Add(this.TextBox_SubnetMask);
+			this.Controls.Add(this.labelIP);
+			this.Controls.Add(this.TextBox_IPAddress);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox TextBox_IPAddress;
+		private System.Windows.Forms.Label labelIP;
+		private System.Windows.Forms.TextBox TextBox_SubnetMask;
+		private System.Windows.Forms.Label labelSubnetMask;
+		private System.Windows.Forms.Button Button_Calculate;
+		private System.Windows.Forms.Label Label_NetworkAddress;
+		private System.Windows.Forms.Label Label_BroadcastAddress;
+		private System.Windows.Forms.Label Label_TotalIPAddresses;
+		private System.Windows.Forms.Label Label_UsableIPAddresses;
+		private System.Windows.Forms.TextBox textBox_NetworkAddress;
+		private System.Windows.Forms.TextBox textBox_BroadcastAddress;
+		private System.Windows.Forms.TextBox textBox_TotalIPAddresses;
+		private System.Windows.Forms.TextBox textBox_UsableIPAddresses;
+	}
+}
+
---
+++ IP_Address/Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP_Address
+{
+	public partial class Main : Form
+	{
+		public Main()
+		{
+			InitializeComponent();
+
+			TextBox_IPAddress.Text = "192.168.1.10";
+			TextBox_SubnetMask.Text = "255.255.255.0";
+			textBox_NetworkAddress.Text = "";
+			textBox_BroadcastAddress.Text = "";
+			textBox_TotalIPAddresses.Text = "";
+			textBox_UsableIPAddresses.Text = "";
+		}
+
+		private void Button_Calculate_Click(object sender, EventArgs e)
+		{
+			string ipAddressString = TextBox_IPAddress.Text;
+			string subnetMaskString = TextBox_SubnetMask.Text;
+
+			IPAddress ipAddress;
+			IPAddress subnetMask;
+
+			if (!IPAddress.TryParse(ipAddressString, out ipAddress))
+			{
+				MessageBox.Show("Неверный формат IP-адреса!", "Ошибка", MessageBoxButtons.OK, 
+					MessageBoxIcon.Error);
+				return;
+			}
+
+			if (!IPAddress.TryParse(subnetMaskString, out subnetMask))
+			{
+				MessageBox.Show("Неверный формат маски подсети!", "Ошибка", MessageBoxButtons.OK, 
+					MessageBoxIcon.Error);
+				return;
+			}
+
+			byte[] ipBytes = ipAddress.GetAddressBytes();
+			byte[] maskBytes = subnetMask.GetAddressBytes();
+
+			if (ipBytes.Length != 4 || maskBytes.Length != 4)
+			{
+				MessageBox.Show("Поддерживается только IPv4!", "Ошибка", MessageBoxButtons.OK, 
+					MessageBoxIcon.Error);
+				return;
+			}
+
+			byte[] networkBytes = new byte[4];
+			for (int i = 0; i < 4; i++)
+			{
+				networkBytes[i] = (byte)(ipBytes[i] & maskBytes[i]);
+			}
+			IPAddress networkAddress = new IPAddress(networkBytes);
+
+			byte[] broadcastBytes = new byte[4];
+			for (int i = 0; i < 4; i++)
+			{
+				// Широковещательный адрес = (IP-адрес ИЛИ (НЕ Маска))
+				// Или проще: Адрес сети + обратный битмаск
+				broadcastBytes[i] = (byte)(networkBytes[i] | (~maskBytes[i] & 0xFF)); 
+				// 0xFF для обрезки до 8 бит
+			}
+			IPAddress broadcastAddress = new IPAddress(broadcastBytes);
+
+			// Количество IP-адресов = 2^(32 - количество единиц в маске)
+			int prefixLength = 0;
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 7; j >= 0; j--)
+				{
+					if ((maskBytes[i] & (1 << j)) != 0)
+					{
+						prefixLength++;
+					}
+				}
+			}
+			int totalAddresses = (int)Math.Pow(2, 32 - prefixLength);
+
+			// Количество узлов = Общее количество адресов - 2 (адрес сети и широковещательный адрес)
+			int usableAddresses = totalAddresses - 2;
+			if (usableAddresses < 0) usableAddresses = 0; // Если сеть /31 или /32, то узлов 0.
+
+			textBox_NetworkAddress.Text = networkAddress.ToString();
+			textBox_BroadcastAddress.Text = broadcastAddress.ToString();
+			textBox_TotalIPAddresses.Text = totalAddresses.ToString();
+			textBox_UsableIPAddresses.Text = usableAddresses.ToString();
+		}
+	}
+}
