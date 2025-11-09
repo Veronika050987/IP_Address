4cd6defd3b0c1cf42ecd092d1b5b93085921e74b
Veronika050987 <veronicaviv@mail.ru>
2025-11-09 15:57:50 +0300

    almost done

--- IPcalculator/main.cpp
+++ IPcalculator/main.cpp
@@ -3,6 +3,8 @@
 #include<CommCtrl.h>
 #include<cstdio>
 #include"resource.h"
+#include<string> // Для std::to_string
+#include<algorithm> // Для std::max
 
 #pragma comment(lib, "Comctl32.lib")
 
@@ -14,6 +16,57 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	return 0;
 }
 
+DWORD GetMaskFromPrefix(DWORD dwPrefix) 
+{
+	if (dwPrefix == 0) return 0;
+	if (dwPrefix > 32) dwPrefix = 32; // Ограничиваем до 32
+	return (0xFFFFFFFF << (32 - dwPrefix));
+}
+
+DWORD GetPrefixFromMask(DWORD dwMask) 
+{
+	if (dwMask == 0) return 0;
+	DWORD prefix = 0;
+	DWORD tempMask = dwMask;
+	// Пересчитываем единицы начиная с самого старшего бита
+	while (prefix < 32 && (tempMask & 0x80000000)) 
+	{
+		prefix++;
+		tempMask <<= 1;
+	}
+	return prefix;
+}
+
+DWORD GetPrefixFromEditBox(HWND hEditPrefix) 
+{
+	CHAR szPrefix[10] = {}; // Достаточный размер для буфера
+	GetDlgItemText(hEditPrefix, IDC_EDIT_PREFIX, szPrefix, sizeof(szPrefix));
+
+	if (strlen(szPrefix) == 0) 
+	{
+		return 0;
+	}
+
+	DWORD prefix = atoi(szPrefix); // Преобразуем строку в число
+
+	if (prefix > 32) prefix = 32;
+
+	return prefix;
+}
+
+void UpdateNetworkFields(HWND hwnd, HWND hIPaddress, HWND hIPmask, HWND hEditPrefix, 
+	HWND hSpinPrefix, DWORD dwIPaddress, DWORD dwNewPrefix) 
+{
+	CHAR szIPprefix[4] = {};
+	sprintf(szIPprefix, "%u", dwNewPrefix);
+	SetDlgItemText(hwnd, IDC_EDIT_PREFIX, szIPprefix);
+	SendMessage(hSpinPrefix, UDM_SETPOS, 0, MAKELPARAM(dwNewPrefix, 0));
+
+	DWORD dwNewMask = GetMaskFromPrefix(dwNewPrefix);
+	SendMessage(hIPmask, IPM_SETADDRESS, 0, dwNewMask);
+}
+
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
@@ -22,7 +75,25 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		InitCommonControls();
 		HWND hSpinPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
-		SendMessage(hSpinPrefix, UDM_SETRANGE, 0, 32);
+		SendMessage(hSpinPrefix, UDM_SETRANGE, 0, MAKELPARAM(32, 0));
+
+		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+
+		DWORD initialIP = 0xC0A80101; // 192.168.1.1
+		SendMessage(hIPaddress, IPM_SETADDRESS, 0, initialIP);
+
+		DWORD initialMask = GetMaskFromPrefix(24);
+		SendMessage(hIPmask, IPM_SETADDRESS, 0, initialMask);
+
+		DWORD dwIPprefix = GetPrefixFromMask(initialMask);
+		CHAR szIPprefix[4] = {}; // Размер 4 для числа до 32 и нулевого символа
+		sprintf(szIPprefix, "%i", dwIPprefix);
+		SetDlgItemText(hwnd, IDC_EDIT_PREFIX, szIPprefix);
+
+		// Синхронизируем спин-контрол с начальным префиксом
+		SendMessage(hSpinPrefix, UDM_SETPOS, 0, MAKELPARAM(dwIPprefix, 0));
 	}
 		break;
 	case WM_COMMAND:
@@ -30,10 +101,12 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
 		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		HWND hSpinPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);// Получаем спин-контрол
+		
 		DWORD dwIPaddress = 0;
 		DWORD dwIPmask = UINT_MAX;
 		DWORD dwIPprefix = 0;
-		CHAR szIPprefix[3] = {};
+		
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
@@ -42,14 +115,13 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			{
 				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
 				DWORD dwFirst = FIRST_IPADDRESS(dwIPaddress);
-				if (dwFirst < 128)dwIPprefix = 8;
-				else if (dwFirst < 192)dwIPprefix = 16;
-				else if (dwFirst < 224)dwIPprefix = 24;
+				if (dwFirst < 128)dwIPprefix = 8;//A
+				else if (dwFirst < 192)dwIPprefix = 16;//B
+				else if (dwFirst < 224)dwIPprefix = 24;//C
+				else dwIPprefix = 32; // Для других (например, multicast)
 
-				sprintf(szIPprefix, "%i", dwIPprefix);
-				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
-				dwIPmask <<= (32 - dwIPprefix);
-				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
+				UpdateNetworkFields(hwnd, hIPaddress, hIPmask, hEditPrefix, hSpinPrefix, 
+					dwIPaddress, dwIPprefix);
 			}
 		}
 			break;
@@ -58,13 +130,70 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
 				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
-				for (; dwIPmask; dwIPmask <<= 1)dwIPprefix++;
-				sprintf(szIPprefix, "&i", dwIPprefix);
-				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
+
+				dwIPprefix = GetPrefixFromMask(dwIPmask);
+				UpdateNetworkFields(hwnd, hIPaddress, hIPmask, hEditPrefix, hSpinPrefix, 
+					dwIPaddress, dwIPprefix);
 			}
 		}
 			break;
+		case IDC_SPIN_PREFIX:
+		{
+			// UDM_GETPOS возвращает текущую позицию спин-контрола.
+			// Low-order word - текущая позиция, High-order word - всегда 0.
+			int currentPrefix = LOWORD(SendMessage(hSpinPrefix, UDM_GETPOS, 0, 0));
+
+			// Получаем текущий IP-адрес, чтобы при необходимости его оставить
+			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+
+			UpdateNetworkFields(hwnd, hIPaddress, hIPmask, hEditPrefix, hSpinPrefix, 
+				dwIPaddress, (DWORD)currentPrefix);			
+		}
+			break;
+		case IDC_EDIT_PREFIX:
+		{
+			// Получаем новый префикс из текстового поля
+			DWORD newPrefix = GetPrefixFromEditBox(hEditPrefix);
+
+			// Получаем текущий IP-адрес
+			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+
+			// Обновляем все поля на основе нового префикса
+			UpdateNetworkFields(hwnd, hIPaddress, hIPmask, hEditPrefix, hSpinPrefix, 
+				dwIPaddress, newPrefix);
+		}
+			break;
 		case IDOK:
+		{
+			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+
+			dwIPprefix = GetPrefixFromEditBox(hEditPrefix);
+
+			CHAR szIPAddrStr[20] = {};
+			CHAR szMaskAddrStr[20] = {};
+			CHAR szPrefixStr[4] = {};
+
+			sprintf(szIPAddrStr, "%d.%d.%d.%d",
+					(dwIPaddress >> 24) & 0xFF,
+					(dwIPaddress >> 16) & 0xFF,
+					(dwIPaddress >> 8) & 0xFF,
+					dwIPaddress & 0xFF);
+
+			sprintf(szMaskAddrStr, "%d.%d.%d.%d",
+					(dwIPmask >> 24) & 0xFF,
+					(dwIPmask >> 16) & 0xFF,
+					(dwIPmask >> 8) & 0xFF,
+					dwIPmask & 0xFF);
+
+			GetDlgItemText(hEditPrefix, IDC_EDIT_PREFIX, szPrefixStr, sizeof(szPrefixStr));
+
+			std::string message = "IP Address: " + std::string(szIPAddrStr) + "\n";
+			message += "Mask: " + std::string(szMaskAddrStr) + "\n";
+			message += "Prefix: /" + std::string(szPrefixStr);
+
+			MessageBox(hwnd, message.c_str(), "Network Info", MB_OK);
+		}
 			break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
