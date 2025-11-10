59b86b2ca1b64024fa8252af403eb44782ce475b
Veronika050987 <veronicaviv@mail.ru>
2025-11-10 19:18:06 +0300

    classwork done

--- IPcalculator/main.cpp
+++ IPcalculator/main.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include<CommCtrl.h>
 #include<cstdio>
@@ -7,6 +7,8 @@
 #pragma comment(lib, "Comctl32.lib")
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+CHAR* FormatAddress(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwAddress);
+CHAR* FormatNumber(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwNumber);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -23,23 +25,32 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		InitCommonControls();
 		HWND hSpinPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
 		SendMessage(hSpinPrefix, UDM_SETRANGE, 0, 32);
+		//https://learn.microsoft.com/en-us/windows/win32/controls/udm-setrange
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
 		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+
 		DWORD dwIPaddress = 0;
 		DWORD dwIPmask = UINT_MAX;
 		DWORD dwIPprefix = 0;
 		CHAR szIPprefix[3] = {};
+
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
 		{
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
+				/*DWORD dwIPaddress = 0;
+				DWORD dwIPmask = UINT_MAX;
+				DWORD dwIPprefix = 0;
+				CHAR szIPprefix[3] = {};*/
+
 				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
 				DWORD dwFirst = FIRST_IPADDRESS(dwIPaddress);
 				if (dwFirst < 128)dwIPprefix = 8;
@@ -52,28 +63,131 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
 			}
 		}
-			break;
-		case IDC_IPMASK:
+		break;
+		/*case IDC_IPMASK:
 		{
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
+				DWORD dwIPmask = UINT_MAX;
+				DWORD dwIPprefix = 0;
+				CHAR szIPprefix[3] = {};
+
 				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
 				for (; dwIPmask; dwIPmask <<= 1)dwIPprefix++;
-				sprintf(szIPprefix, "&i", dwIPprefix);
+				sprintf(szIPprefix, "%i", dwIPprefix);
 				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
 			}
 		}
-			break;
+		break;*/
+		case IDC_EDIT_PREFIX:
+		{
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+
+				/*DWORD dwIPmask = UINT_MAX;
+				DWORD dwIPprefix = 0;
+				CHAR szIPprefix[3] = {};*/
+
+				SendMessage(hEditPrefix, WM_GETTEXT, 3, (LPARAM)szIPprefix);
+				dwIPprefix = atoi(szIPprefix);
+				if (dwIPprefix > 31)
+				{
+					dwIPprefix = 31;
+					strcpy(szIPprefix, "31");
+					SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
+				}
+				dwIPmask <<= (32 - dwIPprefix);
+				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
+			}
+		}
+		break;
 		case IDOK:
-			break;
+		{
+			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+			DWORD dwNetworkAddress = dwIPaddress & dwIPmask;
+			DWORD dwBroadcastAddress = dwIPaddress | ~dwIPmask;
+			DWORD dwCapacity = dwBroadcastAddress - dwNetworkAddress + 1;
+			DWORD dwHosts = dwCapacity - 2;
+
+			CONST INT SIZE = 256;
+			CHAR szInfo[SIZE] = "";
+			CHAR szNetwork[SIZE] = "";
+			CHAR szBroadcast[SIZE] = "";
+			CHAR szCapacity[SIZE] = "";
+			CHAR szHosts[SIZE] = "";
+
+			sprintf
+			(
+				szInfo,
+				"Info:\n%s\n%s\n%s\n%s\n",
+				FormatAddress(szNetwork, "Адрес сети:\t\t\t", dwNetworkAddress),
+				FormatAddress(szBroadcast, "Широковещательный адрес:\t", dwBroadcastAddress),
+				FormatNumber(szCapacity, "Количество IP-адресов:\t", dwCapacity),
+				FormatNumber(szHosts, "Количество узлов:\t\t", dwHosts)
+			);
+			SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)szInfo);
+		}
+		break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 			break;
 		}
 	}
+	break;
+	case WM_NOTIFY:
+	{
+		//if (wParam == IDC_IPMASK)
+		//if(((LPNMHDR)lParam)->code == IPN_FIELDCHANGED)
+		if (((LPNMHDR)lParam)->hwndFrom == GetDlgItem(hwnd, IDC_IPMASK))
+		{
+			HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+			HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+			DWORD dwIPmask = UINT_MAX;
+			DWORD dwIPprefix = 0;
+			CHAR szIPprefix[3] = {};
+
+			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+			for (; dwIPmask >> 31; dwIPmask <<= 1)dwIPprefix++;
+			/////////////////////
+			//dwIPmask = UINT_MAX << (32 - dwIPprefix);
+			//SendMessage(hIPmask, IPM_SETADDRESS, 0, (LPARAM)&dwIPmask);
+			/////////////////////
+
+			sprintf(szIPprefix, "%i", dwIPprefix);
+			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
+		}
 		break;
+	}
+	break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
+
+CHAR* FormatAddress(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwAddress)
+{
+	sprintf
+	(
+		szBuffer,
+		"%s%i.%i.%i.%i;",
+		szMessage,
+		FIRST_IPADDRESS(dwAddress),
+		SECOND_IPADDRESS(dwAddress),
+		THIRD_IPADDRESS(dwAddress),
+		FOURTH_IPADDRESS(dwAddress)
+	);
+	return szBuffer;
+}
+CHAR* FormatNumber(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwNumber)
+{
+	sprintf
+	(
+		szBuffer,
+		"%s%i;",
+		szMessage,
+		dwNumber
+	);
+	return szBuffer;
 }
